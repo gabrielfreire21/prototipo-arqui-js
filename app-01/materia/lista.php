4504c54af607ddc21682bad80ec121b298193e83
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-05-28 12:56:11 -0300

    primeira app

---
+++ app-01/materia/lista.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * View da lista de matérias
+ * 
+ */
+
+
+require "../biblio/classes/Conn.class.php";
+require "../biblio/classes/HTMLcombo.php";
+require "../biblio/models/Materia.model.php";
+
+$objMateria   = new MateriaModel();
+$arr_materias = $objMateria->getObjects();
+
+?>
+<button type="button" class="btn btn-primary" id="lista-btn-inserir" 
+        style="margin: 10px; margin-left: 45%;">Inserir</button>
+<table class="table">
+<tr>
+    <th>id</th>
+    <th>título</th>
+    <th>autor</th>
+    <th>secao</th>
+    <th>data atuali.</th>
+    <th>deletar</th>    
+</tr>
+<?php foreach ($arr_materias as $materia): ?>
+    <tr id="mt-<?php echo $materia->id?>">
+        <td><?php echo $materia->id ?></td>
+        <td>
+            <a href="#" >
+                <?php echo $materia->titulo ?>
+            </a>
+        </td>
+        <td><?php echo $materia->autor ?></td>
+        <td><?php echo $materia->secao ?></td>
+        <td><?php echo $materia->dt_atualizacao ?></td>
+        <td><button type="button" class="btn btn-danger btn-mini" >Excluir</button></td>
+    </tr>
+<?php endforeach; ?>
+</table>
