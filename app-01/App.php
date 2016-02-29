71915c9b7697c9d61b2e715afe5ff2ff8c4743a9
Flávio Micheletti <flaviomicheletti@hotmail.com>
2016-02-29 11:14:28 -0300

    App.php

--- app-01/App.php
+++ app-01/App.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 'on');
+ini_set('track_errors', 'on');
+ini_set('html_errors', 'on');
+ini_set('error_reporting', E_ALL);
+error_reporting(E_ALL);
+
 /**
  * Arquivo bootstrap
  */
@@ -8,7 +14,7 @@
  *
  */
 session_start();
-
+date_default_timezone_set('America/Sao_Paulo');
 
 /**
 * Caminho relativo da raiz do sistema
